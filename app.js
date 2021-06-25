17b441171ba8b4d664d63c1d95533f65b3d6e17e
student <student@sec588.com>
2021-06-25 10:32:13 +0000

    blah4

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
 app.get('/', function (req, res) {
-  res.send('Im a helpful app!');
+  res.send('Im an helpful app!');
 });
 app.listen(3001, function () {
   console.log('Example app listening on port 3000!');
